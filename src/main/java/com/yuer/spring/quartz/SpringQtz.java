220153c40a3f6825315c2670f5aa04e2516b0ee4
lei.lin <lei.lin@cosunet.com.cn>
2017-01-12 12:42:44 +0800

    集成quartz任务调度模块，并提供demo案例

---
+++ src/main/java/com/yuer/spring/quartz/SpringQtz.java
@@ -0,0 +1,26 @@
+package com.yuer.spring.quartz;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class SpringQtz {
+	
+	private static int count = 0;
+	
+	protected void execute() throws InterruptedException{
+//		long ms = System.currentTimeMillis();
+//		Calendar calendar = Calendar.getInstance();
+//		calendar.setTime(new Date(ms));
+		
+		System.out.println("(" + count++ + ")");
+		System.out.println(Thread.currentThread().getName() + "正在执行查询...");
+		Thread.sleep(5000);
+		System.out.println(Thread.currentThread().getName() + "正在执行删除...");
+		Thread.sleep(5000);
+		System.out.println(Thread.currentThread().getName() + "删除成功...");
+		Thread.sleep(5000);
+		
+	}
+	
+	
+}
